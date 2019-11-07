df033a2d2444c1201e9749ae69cdb7489bc02deb
Edward Hill <edwardpassanohill@gmail.com>
2019-11-07 15:38:22 -0500

    Change to fishing word guess game, add images, copy to match HW instructions, columns for text and images

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+/*
+
+event listener 
+
+1. alert - press any key to get started
+2. Display # of wins: (# of times user guessed the word correctly).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var hownWord = "_______"
+var hiddenWord = "Hangman"
+var shownWord = "_an____n"
+var wins = 0
+var losses = 0
+
+function reserBoard {
+    var shown
+}
+
+funtion handleUserGuess {
+
+} */
+
+/* alert("Press any key to get started");*/
